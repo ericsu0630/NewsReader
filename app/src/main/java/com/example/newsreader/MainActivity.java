cf1a127064d07ddcb3c020d3f62bb6864720ee53
Eric Su <ericsu0630@gmail.com>
2020-09-03 16:33:51 +0800

    third commit, added progressbar

--- app/src/main/java/com/example/newsreader/MainActivity.java
+++ app/src/main/java/com/example/newsreader/MainActivity.java
@@ -7,9 +7,11 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.InputStream;
@@ -20,40 +22,35 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView listView;
     ArrayList<Story> stories = new ArrayList<>();
+    ArrayList<String> titles = new ArrayList<>();
+    ArrayAdapter<String> arrayAdapter;
+    ProgressBar progressBar, loadingSpinner;
+    JSONArray articleIDs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView listView = findViewById(R.id.listView);
-        ArrayList<String> titles = new ArrayList<>();
+        listView = findViewById(R.id.listView);
+        progressBar = findViewById(R.id.progressBar);
+        loadingSpinner = findViewById(R.id.loadingSpinner);
         DownloadArticles downloadArticles = new DownloadArticles();
+        arrayAdapter =  new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,titles);
+        listView.setAdapter(arrayAdapter);
+        progressBar.setMax(20);
 
         try {
-            stories = downloadArticles.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty").get();
+            downloadArticles.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
         } catch (Exception e) {
             e.printStackTrace();
-            Log.i("Warning", "Download failed!");
+            Log.i("Error", "Download failed!");
         }
 
-        for(int i = 0; i < stories.size(); i++){
-            titles.add(stories.get(i).title);
-        }
-
-        ArrayAdapter<String> arrayAdapter =  new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,titles);
-        listView.setAdapter(arrayAdapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(getApplicationContext(),WebViewActivity.class);
-                intent.putExtra("url", stories.get(position).url);
-                startActivity(intent);
-            }
-        });
     }
 
-    //Returns the JSON data retrieved from a URL
+    //Method that returns the JSON data retrieved from a URL
     public String getJSON(String u){
         StringBuilder json = new StringBuilder();
         try{
@@ -70,40 +67,95 @@ public class MainActivity extends AppCompatActivity {
             }while(data!=-1);
         }catch(Exception e){
             e.printStackTrace();
-            Log.i("Warning", "GET JSON failed!");
+            Log.i("Error", "GET JSON failed!");
         }
         return String.valueOf(json);
+    }
+
+    //Method to fetch the next 20 articles and add them to the listView
+    public void loadArticles(){
+        String title, jsonUrl, contentUrl;
+        try {
+            for (int i = titles.size(); i < titles.size()+20; i++) { //downloads the top 20 stories out of 500 article IDs
+                jsonUrl = "https://hacker-news.firebaseio.com/v0/item/" + articleIDs.get(i) + ".json?print=pretty";
+                String json = getJSON(jsonUrl); //gets article data using article ID
+                JSONObject storyData = new JSONObject(json);
+
+                //if the article doesn't contain a title or url then skip it
+                if (storyData.has("url") && storyData.has("title")) {
+                    title = storyData.getString("title");
+                    contentUrl = storyData.getString("url");
+                    Story story = new Story(title, contentUrl); //construct a new story with article title and url
+                    stories.add(story);
+                }
+                progressBar.setProgress(i-titles.size());
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+            Log.i("Error", "JSON handling failed");
+        }
+    }
+
+    //Method to populate the listView
+    public void loadTitles(){
+        for(int i = 0; i < stories.size(); i++){
+            titles.add(stories.get(i).title);
+        }
+        titles.add("More stories...");
     }
 
     //Downloads the top 10 article IDs from a list of 500
     public class DownloadArticles extends AsyncTask<String, Void, ArrayList<Story>>{
 
         @Override
+        protected void onPreExecute() {
+            arrayAdapter.notifyDataSetChanged();
+            loadingSpinner.setVisibility(View.VISIBLE);
+        }
+
+        @Override
         protected ArrayList<Story> doInBackground(String... strings) {
-            ArrayList<Story> stories = new ArrayList<>();
-                String json = getJSON(strings[0]); //gets list of article IDs
-                String title, jsonUrl, contentUrl;
+            String json = getJSON(strings[0]); //gets list of 500 article IDs
             try {
-                JSONArray articleIDs = new JSONArray(String.valueOf(json));
-                for(int i=0;i<20;i++){ //downloads the top ten stories out of 500 article IDs
-                    jsonUrl = "https://hacker-news.firebaseio.com/v0/item/"+articleIDs.get(i)+".json?print=pretty";
-                    json = getJSON(jsonUrl); //gets article data using article ID
-                    JSONObject storyData = new JSONObject(json);
-                    //if the article doesn't contain a title or url then skip it
-                    if(storyData.has("url") && storyData.has("title")){
-                        title = storyData.getString("title");
-                        contentUrl = storyData.getString("url");
-                        Story story = new Story(title, contentUrl); //construct a new story with article title and url
-                        stories.add(story);
-                    }
-                }
+                articleIDs = new JSONArray(String.valueOf(json)); //stores list of 500 article IDs
+                Log.i("Article IDs", articleIDs.toString());
             } catch (Exception e) {
                 e.printStackTrace();
-                Log.i("Warning","JSON handling failed");
+                Log.i("Error","Store JSON failed");
             }
+            loadArticles();
             return stories;
         }
 
+        @Override
+        protected void onPostExecute(ArrayList<Story> s) {
+            stories = s;
+            arrayAdapter.notifyDataSetChanged();
+            listView.setVisibility(View.VISIBLE);
+            progressBar.setVisibility(View.GONE);
+            loadingSpinner.setVisibility(View.GONE);
+            loadTitles();
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                    if(position==titles.size()-1){ //if the last item is clicked, load more data
+                        Log.i("debug info","Loading more items...");
+                        titles.remove(position);
+
+                        //loadArticles()
+
+                        arrayAdapter.notifyDataSetChanged();
+                    }else {
+                        Log.i("debug info","opening webView...");
+                        Intent intent = new Intent(getApplicationContext(), WebViewActivity.class);
+                        intent.putExtra("url", stories.get(position).url);
+                        startActivity(intent);
+                    }
+
+                }
+            });
+        }
     }
 
     //Story constructor class
